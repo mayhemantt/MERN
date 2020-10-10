09717228f7e9c5950cafe442a831e6c4db0f6418
Hemant Joshi <hd4207577@gmail.com>
2020-10-10 08:39:55 +0530

    :Prettier

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
+  const handleSubmit = () =>{
+    e.preventDefault()
+    const config={
+        url=""
+    }
+  }
+  const [email, setEmail] = useState("");
+  const registerForm = () => (
+    <form className='mt-5' onSubmit={handleSubmit}>
+      <input
+        placeholder='E Mail'
+        type='email'
+        className='form-control'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
+      <button type='submit' className='btn btn-raised mt-3'>
+        Register
+      </button>
+    </form>
+  );
   return (
     <div>
-      <div className="container p-5">
-        <div className="row">
-          <div className="col-md-6 offset-md-3">
-<h4>Register</h4>
-<p>Register Form</p>
+      <div className='container p-5'>
+        <div className='row'>
+          <div className='col-md-6 offset-md-3'>
+            <h4>Register</h4>
+            {registerForm()}
           </div>
         </div>
       </div>
     </div>
-    
-    
   );
 }
 
