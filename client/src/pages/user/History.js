9b1f0ca9749f7f56c53906a03e3f755328b5c344
Hemant Joshi <hd4207577@gmail.com>
2020-11-07 12:52:02 +0530

    text to pdf

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -4,6 +4,17 @@ import { getUserOrders } from "../../functions/user";
 import { useSelector, useDispatch } from "react-redux";
 import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import { toast } from "react-toastify";
+import ShowPaymentInfo from "../../components/cards/ShowPaymentInfo";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFDownloadLink,
+  PDFViewer,
+} from "@react-pdf/renderer";
+import Invoice from "../../components/order/invoice";
 
 const History = () => {
   const [orders, setOrders] = useState([]);
@@ -15,9 +26,65 @@ const History = () => {
 
   const loadUserOrders = () =>
     getUserOrders(user.token).then((res) => {
-      // console.log(JSON.stringify(res.data, null, 4));
+      console.log(JSON.stringify(res.data, null, 4));
       setOrders(res.data);
     });
+
+  const showOrderInTable = (order) => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.title}</b>
+            </td>
+            <td>{p.product.price}</td>
+            <td>{p.product.brand}</td>
+            <td>{p.color}</td>
+            <td>{p.count}</td>
+            <td>
+              {p.product.shipping === "Yes" ? (
+                <CheckCircleOutlined style={{ color: "green" }} />
+              ) : (
+                <CloseCircleOutlined style={{ color: "red" }} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  const showEachOrders = () =>
+    orders.map((order, i) => (
+      <div key={i} className="m-5 p-3 card">
+        <ShowPaymentInfo order={order} />
+        {showOrderInTable(order)}
+        <div className="row">
+          <div className="col">{showDownloadLink(order)}</div>
+        </div>
+      </div>
+    ));
+
+  const showDownloadLink = (order) => (
+    <PDFDownloadLink
+      document={<Invoice order={order} />}
+      fileName="invoice.pdf"
+      className="btn btn-sm btn-block btn-outline-primary">
+      Download PDF
+    </PDFDownloadLink>
+  );
 
   return (
     <div className="container-fluid">
@@ -25,10 +92,11 @@ const History = () => {
         <div className="col-md-2">
           <UserNav />
         </div>
-        <div className="col">
-          user history page
-          <hr />
-          {JSON.stringify(orders)}
+        <div className="col text-center">
+          <h4>
+            {orders.length > 0 ? "User purchase orders" : "No purchase orders"}
+          </h4>
+          {showEachOrders()}
         </div>
       </div>
     </div>
